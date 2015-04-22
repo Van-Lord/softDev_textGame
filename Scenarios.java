d1c080af9aa3f29d38e67f3c5a6e400d24f456c0
Van-Lord <vanjinn@gmail.com>
2015-04-22 13:30:22 -0400

    scenarios updatwd

--- Scenarios.java
+++ Scenarios.java
@@ -9,21 +9,15 @@ public class Scenarios {
 			+ "\nAs the ship lands it is determined that a group of 4 will exit the space craft and search the planet. "
 			+ "\nThe plane lands and the group of 4 exit the craft, leaving the others on the craft. "
 			+ "\nAll the followers go into different directions. "
-			+ "\nYou are standing on the grassy plain and the only way for you to go is west."; 
+			+ "\nYou are standing on the grassy plain and the only way for you to go is west.";
 
-	
-	
 	public static final String ROOM2 = "Walking west of the spacecraft, the YOU admire the beauty of the planet. "
 			+ "\nAs you continue to walk your team runs into a mountain covered with vines of blue and aqua as well as blue trees with indigo leaves and violet trees. "
 			+ "\nLooking at the mountain straight-forward you see three entrances and all of them have different marking above the entrance. "
 			+ "\nThe left entrance is curved in shape and has an emblem right above it. The middle entrance is square in shape and has a group of bees above it. "
 			+ "\nThe right entrance is triangle in shape and has a stream of glowing plasma flowing above the “V” shaped entrance."
-			+ "\n[1] GO LEFT"
-			+ "\n[2] GO RIGHT"
-			+ "\n[3] GO STRAIGHT";
-	
-	
-	
+			+ "\n[1] GO LEFT" + "\n[2] GO RIGHT" + "\n[3] GO STRAIGHT";
+
 	public static final String ROOM3 = "Walking to the left, you walk through the curved shaped entrance into the pitch black cave. "
 			+ "\nAs you pass the entrance a door made of rock slams shut behind you, trapping you in the cave.  "
 			+ "\nYou become agitated with the occurrence and continues to walk forward. "
@@ -32,58 +26,96 @@ public class Scenarios {
 			+ "\nFinally reaching the end of the cave, you are now met with a door with flashing plasma balls and a panel on the right side. "
 			+ "The balls flash in a certain order blue, blue, aqua, green, purple, green. The flashing starts again: aqua, purple... "
 			+ "\nThe flashing stops and then starts back again with two blue. ";
-	
-	public static final String ROOM4 ="Another door opens up and reveals the other side of the mountain. "
+
+	public static final String ROOM4 = "Another door opens up and reveals the other side of the mountain. "
 			+ "\nYou move forward and the door slams shut behind. "
 			+ "\nyou, run in to the spaceship again and the road west back towards the entrance to the mountain. "
 			+ "\n(You made a huge circle and must continue around in another entrance. "
 			+ "\nGo west and Into another entrance ";
-	
-	public static final String ROOM5 ="The player takes the right entrance with plasma. "
+
+	public static final String ROOM5 = "The player takes the right entrance with plasma. "
 			+ "\nOnce, the you walk past the entrance a field of plasma stretches over "
 			+ "\nthe entrance and the cave is now lit by plasma streams along the wall. "
 			+ "\nThe cave is decorated with mossy rocks and the floor is covered with purple grass. "
 			+ "\nAs the you are walking down the plasma lit cave, you run into a room with a floating blue light.";
-	
-	//a.Continue on through the cave
-	//b.Go into the room 
-	
-	public static final String ROOM6 ="After entering the room, you decide, "
+
+	// a.Continue on through the cave
+	// b.Go into the room
+
+	public static final String ROOM6 = "After entering the room, you decide, "
 			+ "nothing is in the room and get a weird feeling about leaving. ";
-	
-	//a.Go further into the room 
-	//b.Leave the room immediately
-	
-	public static final String ROOM6_2 ="You listen to your gut and start back for the entrance. "
+
+	// a.Go further into the room
+	// b.Leave the room immediately
+
+	public static final String ROOM6_2 = "You listen to your gut and start back for the entrance. "
 			+ "The entrance begins to get farther and farther away. "
 			+ "The room closes in on you and you sufocate. {Game over}  ";
-	
-	public static final String ROOM7 ="";
-	public static final String ROOM8 ="";
-	public static final String ROOM9 ="";
-	public static final String ROOM10 ="";
-	public static final String ROOM11 ="";
-	public static final String ROOM12 ="";
-	public static final String ROOM13 ="";
-	public static final String ROOM14 ="";
-	public static final String ROOM15 ="";
-	public static final String ROOM16 ="";
-	public static final String ROOM17 ="";
-	public static final String ROOM18 ="";
-	public static final String ROOM19 ="";
-	public static final String ROOM20 ="";
-	public static final String ROOM21 ="";
-	public static final String ROOM22 ="";
-	public static final String ROOM23 ="";
-	public static final String ROOM24 ="";
-	public static final String ROOM25 ="";
-	public static final String ROOM26 ="";
-	public static final String ROOM27 ="";
-	
-	
-	public static final String ROOM28 ="";
-	public static final String ROOM29 ="";
-	public static final String ROOM30 ="";
 
+	public static final String ROOM7 = "The player decides to stay walking further into the room and comes face to face with the light. "
+			+ "\nThe light shatters and the room begins to close in on the player."
+			+ "\nThe pieces of light surround the player and then move in front of the player. "
+			+ "\nA message of light begins to appear and the message turns out to be a riddle."
+			+ "\nRIDDLE: There will be two different riddles. ";
+
+	// riddle puzzle
+	public static final String ROOM7_2 = "After solving the riddle the room doubles in size and lights blue and green in color surround the room."
+			+ "\nNow in the room there is a box of sorts and the entrance opens up for the player to exit."
+			+ "\n It might be a good idea to check the box.";
+
+	public static final String CHECK_BOX = "In the box there are bottles of different color liquids, a mask and a weapon. "
+			+ "\nType 'add' to add the items to your inventory. ";
+
+	public static final String ROOM8 = "Continuing down the cave, the player is introduced to a plethora of plants all of which are omitting different smells. "
+			+ "\nOne particular plant catches the player’s eye. "
+			+ "\nThe plant is pinks in color with purple stripes, blue dots and resembles a dahlia flower. "
+			+ "\nThe player decides to ignore the smell and continues down the cave and runs into Jack Sheppard from the space ship."
+			+ "\nJack is freaking out for some reason and speaking in riddles. He begins to get mad at you for not understanding him and swings at the player. "
+			+ "\nThe player ducks the hit just in time but then a tail begins to show from behind Jack. "
+			+ "\nThe player notices and tries to run in the opposite direction but is then hit on the head. "
+			+ "\nThe player  passes out from the blow and is carried away by Jack.  ";
+
+	public static final String ROOM9 = "The player wakes up in a cage and hears cheering. "
+			+ "\n to open the cage you need to remember the answer from the past riddlles ";
+
+	// Memorisatioon puzzle
+
+	public static final String ROOM9_2 = " Finally out of the cage the player runs to the items that were once in his possession and places them back into his back pack. "
+			+ "\nThe player looks around the room and notices a black door as well as a little brown vent on the other side of the room. ";
+	// enter vent
+
+	public static final String ROOM10 = "The player goes for the vent and is lead down a dark room. "
+			+ "\nThe room feels like it is getting longer and longer as the player walks down. "
+			+ "\nThe player gets to a point in the room where there are two glowing items. "
+			+ "\nOne of the items looks like a door handle and the other one button of sorts. ";
+
+	// puzzle ButtonHandle
+
+	public static final String ROOM11 = "The player pulls the handle to reveal another room with pink lighting. "
+			+ "\nEntering the room the player notices a weird smell similar to bad body odor. "
+			+ "\nJust as the player turns their nose, the door to the room slams shut and a giant Plant monster begins to attack. ";
+	// fight monster
+
+	public static final String ROOM112 = "After killing the monster you exit to the outside of the cave which in turn leads back to the spaceship. "
+			+ "\nThe player realizes the dangers of the planet and goes back to the entrance of the cave to get the other crew members.";
+
+	//pick another entrance
 	
+	public static final String ROOM16 = "";
+	public static final String ROOM17 = "";
+	public static final String ROOM18 = "";
+	public static final String ROOM19 = "";
+	public static final String ROOM20 = "";
+	public static final String ROOM21 = "";
+	public static final String ROOM22 = "";
+	public static final String ROOM23 = "";
+	public static final String ROOM24 = "";
+	public static final String ROOM25 = "";
+	public static final String ROOM26 = "";
+	public static final String ROOM27 = "";
+
+	public static final String ROOM28 = "";
+	public static final String ROOM29 = "";
+	public static final String ROOM30 = "";
+
 }
