36a2f926ab7d3f0e41f6a5a9c20b0fee6a4c7015
Van-Lord <vanjinn@gmail.com>
2015-04-24 15:40:16 -0400

    final scenario

--- Scenarios.java
+++ Scenarios.java
@@ -1,24 +1,24 @@
-
+package thatNewWay;
 
 public class Scenarios {
 
-	public static final String SENARIO_1 = "A group of travelers make way to the newly found planet of unknown name. "
+	public static final String SCENARIO_1 = "A group of travelers make way to the newly found planet of unknown name. "
 			+ "\nThe planet is covered with massive forest and grassy terrain. "
-			+ "\nA plethora of colors excite the travelerï¿½s eyes as they fly over the plant to find a landing place.  "
+			+ "\nA plethora of colors excite the traveler’s eyes as they fly over the plant to find a landing place.  "
 			+ "\nMixture of blues, purples, indigos, violets, and aqua cover the planet in its beauty. "
 			+ "\nAs the ship lands it is determined that a group of 4 will exit the space craft and search the planet. "
 			+ "\nThe plane lands and the group of 4 exit the craft, leaving the others on the craft. "
 			+ "\nAll the followers go into different directions. "
-			+ "\nYou are standing on the grassy plain and the only way for you to go is west. \n[1]GO WEST";
+			+ "\nYou are standing on the grassy plain and the only way for you to go is west.";
 
-	public static final String SENARIO_2 = "Walking west of the spacecraft, the YOU admire the beauty of the planet. "
+	public static final String SCENARIO_2 = "Walking west of the spacecraft, the YOU admire the beauty of the planet. "
 			+ "\nAs you continue to walk your team runs into a mountain covered with vines of blue and aqua as well as blue trees with indigo leaves and violet trees. "
 			+ "\nLooking at the mountain straight-forward you see three entrances and all of them have different marking above the entrance. "
 			+ "\nThe left entrance is curved in shape and has an emblem right above it. The middle entrance is square in shape and has a group of bees above it. "
-			+ "\nThe right entrance is triangle in shape and has a stream of glowing plasma flowing above the ï¿½Vï¿½ shaped entrance."
+			+ "\nThe right entrance is triangle in shape and has a stream of glowing plasma flowing above the “V” shaped entrance."
 			+ "\n[1] GO LEFT" + "\n[2] GO RIGHT" + "\n[3] GO STRAIGHT";
 
-	public static final String SENARIO_3 = "Walking to the left, you walk through the curved shaped entrance into the pitch black cave. "
+	public static final String SCENARIO_3 = "Walking to the left, you walk through the curved shaped entrance into the pitch black cave. "
 			+ "\nAs you pass the entrance a door made of rock slams shut behind you, trapping you in the cave.  "
 			+ "\nYou become agitated with the occurrence and continues to walk forward. "
 			+ "\nContinuing the walk down the dark cave, you see a thin white light and begin to run towards it. "
@@ -26,14 +26,14 @@ public class Scenarios {
 			+ "\nFinally reaching the end of the cave, you are now met with a door with flashing plasma balls and a panel on the right side. "
 			+ "The balls flash in a certain order blue, blue, aqua, green, purple, green. The flashing starts again: aqua, purple... "
 			+ "\nThe flashing stops and then starts back again with two blue. ";
-
-	public static final String SENARIO_4 = "Another door opens up and reveals the other side of the mountain. "
+//lightcombination
+	public static final String SCENARIO_4 = "Another door opens up and reveals the other side of the mountain. "
 			+ "\nYou move forward and the door slams shut behind. "
 			+ "\nyou, run in to the spaceship again and the road west back towards the entrance to the mountain. "
 			+ "\n(You made a huge circle and must continue around in another entrance. "
 			+ "\nGo west and Into another entrance ";
 
-	public static final String SENARIO_5 = "The player takes the right entrance with plasma. "
+	public static final String SCENARIO_5 = "The player takes the right entrance with plasma. "
 			+ "\nOnce, the you walk past the entrance a field of plasma stretches over "
 			+ "\nthe entrance and the cave is now lit by plasma streams along the wall. "
 			+ "\nThe cave is decorated with mossy rocks and the floor is covered with purple grass. "
@@ -42,32 +42,32 @@ public class Scenarios {
 	// a.Continue on through the cave
 	// b.Go into the room
 
-	public static final String SENARIO_6 = "After entering the room, you decide, "
+	public static final String SCENARIO_6 = "After entering the room, you decide, "
 			+ "nothing is in the room and get a weird feeling about leaving. ";
 
 	// a.Go further into the room
 	// b.Leave the room immediately
 
-	public static final String ROOM_6_2 = "You listen to your gut and start back for the entrance. "
+	public static final String SCENARIO_6_2 = "You listen to your gut and start back for the entrance. "
 			+ "The entrance begins to get farther and farther away. "
 			+ "The room closes in on you and you sufocate. {Game over}  ";
 
-	public static final String SENARIO_7 = "The player decides to stay walking further into the room and comes face to face with the light. "
+	public static final String SCENARIO_7 = "The player decides to stay walking further into the room and comes face to face with the light. "
 			+ "\nThe light shatters and the room begins to close in on the player."
 			+ "\nThe pieces of light surround the player and then move in front of the player. "
 			+ "\nA message of light begins to appear and the message turns out to be a riddle."
 			+ "\nRIDDLE: There will be two different riddles. ";
 
 	// riddle puzzle
-	public static final String ROOM7_2 = "After solving the riddle the room doubles in size and lights blue and green in color surround the room."
+	public static final String SCENARIO_7_2 = "After solving the riddle the room doubles in size and lights blue and green in color surround the room."
 			+ "\nNow in the room there is a box of sorts and the entrance opens up for the player to exit."
 			+ "\n It might be a good idea to check the box.";
 
 	public static final String CHECK_BOX = "In the box there are bottles of different color liquids, a mask and a weapon. "
 			+ "\nType 'add' to add the items to your inventory. ";
 
-	public static final String ROOM8 = "Continuing down the cave, the player is introduced to a plethora of plants all of which are omitting different smells. "
-			+ "\nOne particular plant catches the playerï¿½s eye. "
+	public static final String SCENARIO_8 = "Continuing down the cave, the player is introduced to a plethora of plants all of which are omitting different smells. "
+			+ "\nOne particular plant catches the player’s eye. "
 			+ "\nThe plant is pinks in color with purple stripes, blue dots and resembles a dahlia flower. "
 			+ "\nThe player decides to ignore the smell and continues down the cave and runs into Jack Sheppard from the space ship."
 			+ "\nJack is freaking out for some reason and speaking in riddles. He begins to get mad at you for not understanding him and swings at the player. "
@@ -75,47 +75,124 @@ public class Scenarios {
 			+ "\nThe player notices and tries to run in the opposite direction but is then hit on the head. "
 			+ "\nThe player  passes out from the blow and is carried away by Jack.  ";
 
-	public static final String ROOM9 = "The player wakes up in a cage and hears cheering. "
+	public static final String SCENARIO_9 = "The player wakes up in a cage and hears cheering. "
 			+ "\n to open the cage you need to remember the answer from the past riddlles ";
 
 	// Memorisatioon puzzle
 
-	public static final String ROOM9_2 = " Finally out of the cage the player runs to the items that were once in his possession and places them back into his back pack. "
+	public static final String SCENARIO_9_2 = " Finally out of the cage the player runs to the items that were once in his possession and places them back into his back pack. "
 			+ "\nThe player looks around the room and notices a black door as well as a little brown vent on the other side of the room. ";
 	// enter vent
 
-	public static final String ROOM10 = "The player goes for the vent and is lead down a dark room. "
+	public static final String SCENARIO_10 = "The player goes for the vent and is lead down a dark room. "
 			+ "\nThe room feels like it is getting longer and longer as the player walks down. "
 			+ "\nThe player gets to a point in the room where there are two glowing items. "
 			+ "\nOne of the items looks like a door handle and the other one button of sorts. ";
 
 	// puzzle ButtonHandle
 
-	public static final String ROOM11 = "The player pulls the handle to reveal another room with pink lighting. "
+	public static final String SCENARIO_11 = "The player pulls the handle to reveal another room with pink lighting. "
 			+ "\nEntering the room the player notices a weird smell similar to bad body odor. "
 			+ "\nJust as the player turns their nose, the door to the room slams shut and a giant Plant monster begins to attack. ";
 	// fight monster
 
-	public static final String ROOM112 = "After killing the monster you exit to the outside of the cave which in turn leads back to the spaceship. "
+	public static final String SCENARIO_12 = "After killing the monster you exit to the outside of the cave which in turn leads back to the spaceship. "
 			+ "\nThe player realizes the dangers of the planet and goes back to the entrance of the cave to get the other crew members.";
 
+	//here are first 10 rooms (12 speificaly)
 	//pick another entrance
 	
-	public static final String ROOM16 = "";
-	public static final String ROOM17 = "";
-	public static final String ROOM18 = "";
-	public static final String ROOM19 = "";
-	public static final String ROOM20 = "";
-	public static final String ROOM21 = "";
-	public static final String ROOM22 = "";
-	public static final String ROOM23 = "";
-	public static final String ROOM24 = "";
-	public static final String ROOM25 = "";
-	public static final String ROOM26 = "";
-	public static final String ROOM27 = "";
-
-	public static final String ROOM28 = "";
-	public static final String ROOM29 = "";
-	public static final String ROOM30 = "";
-
+	public static final String SCENARIO_13 = "The player decides to go through the door and is lead to another room filled with red lighting and what looks like mounted trophies on the walls. "
+			+ "\nThe player continues down the room and notices that the head on the trophies look like human beings. "
+			+ "\nThe player proceeds down the room and is lead to another door. ";
+	//smelling roses
+	public static final String SCENARIO_14 = "The player goes through the door in front of them."
+			+ "\nOpening the door, the player is met with even louder cheering and decides to investigate. "
+			+ "\nWalking out into the new room the player is in the process of looking around the room,"
+			+ "\n when they run into a large alien monster which is about four times their size";
+	//smelling roses
+	public static final String SCENARIO_15 = "The player goes through the door in front of them. "
+			+ "\nOpening the door, the player is met with even louder cheering and decides to investigate. "
+			+ "\nWalking out into the new room the player is in the process of looking around the room "
+			+ "\nwhen they run into a large alien monster which is about four times their size";
+	//fight monster
+	public static final String SCENARIO_16 = "The door leads to an open field similar to a coliseum and an announcer begins to talk in a language unknown to the player. "
+			+ "\nAfter the announcer finished, four gates open up and coming out of all of the gates are different colors of light. "
+			+ "\nA weapon is thrown at the player and they are pushed towards the four gates."
+			+ "\n[1]PINK "
+			+ "\n[2]PURPLE "
+			+ "\n[3]BLUE "
+			+ "\n[4]GREEN";
+	
+	public static final String SCENARIO_17_PINK = "The pink light leads to a pretty room completely decorated with pinks and floral plants and flowers. "
+			+ "\nThe player walks further into the lair and an alien about the same size of the player "
+			+ "\nwhich is dressed in a pink wedding dress with a staff made from pink flowers. "
+			+ "\nShe begins to speak but the player does not understand her. "
+			+ "\nThe alien stares deeply into the player’s eye. "
+			+ "\nAfter about two minutes the player is placed back on the floor and the alien begins to speak in Spanish"
+			+ "\n which is the player’s native tongue. "
+			+ "\nThe alien tells the player that they have to beat all of the monsters in the four rooms to be able to leave. "
+			+ "\nShe also continued on that her challenge was a puzzle.";
+	//puzzle alien necklace 
+	//pick another room
+	
+	public static final String SCENARIO_18_PURPLE = "The purple room is filled with a number of aliens all of which attack the player. "
+			+ "\nThe player dodges the aliens which all then begin to form one medium sized alien monster. "
+			+ "\nThe alien speaks in Spanish and says “Die!”";
+	//monster fight
+	//pick another room
+	
+	public static final String SCENARIO_19_BLUE = "The blue room is filled with water and a walk way ofmidnight blue vines. "
+			+ "\nAs the player approaches the middle of the room, they hear a splash and then they hear a couple more splashes. "
+			+ "\nThe splashes stop and a large blue alien jumps out of the water. "
+			+ "\nHe also begins to speak in Spanish about he is the blue god of the water on this planet which he calls Extranjero. "
+			+ "\nThen the alien begins to tell you his challenge which is a series of riddles. "
+			+ "\nThe alien informs the player that he is not there to eat him but will whenever the player gets a wrong answer.";
+//TrippleRidle puzzle
+//pick another room
+	
+	public static final String SCENARIO_20_GREEN = "The green room is surrounded by trees and vines of different colors of green. "
+			+ "\nThe player notices an alien in the distance sitting down meditating. "
+			+ "\nWhen the player steps on the grass the alien jumps from where he is and attacks the player.";
+	//monster fight
+	//get out completed all color rooms.
+	
+	public static final String SCENARIO_21 = "The player has defeated all of the aliens and is now back in the coliseum. "
+			+ "\nThe announcer gets mad and screams in another language. "
+			+ "\nAfter the announcer is done all of the participants fly away and the player is left alone. "
+			+ "\nThen the player is transported (magically) back outside the space ship. "
+			+ "\nThe player realizes the dangers of the planet and goes back to the entrance of the cave to get the other crew members.";
+//pick another entrance the last one.
+	
+	//another 10 rooms , with second scenarios
+	public static final String SCENARIO_22 = "The cave with the bees lead to a large opening "
+			+ "\nand the player finds themselves inside of a large bee hive. "
+			+ "\nThe player is then surrounded by a couple of bees. "
+			+ "\nThe bees do not harm the player but they do begin to lead them further into the hive. ";
+	//follow the bees.
+	
+	public static final String SCENARIO_23 = "Following the rad the bees leads the player to a large room with yellow honey combs everywhere. "
+			+ "\nThe player begins to search the room when a loud buzz is heard. "
+			+ "\nAfter the buzz the player hears a loud thump and the queen bee is now right behind them. "
+			+ "\nThe queen notices the player and goes to attack. ";
+	//fight the queen.
+	
+	public static final String SCENARIO_24 = "The player goes for the room which is lit with different colors of plasma. "
+			+ "\nWithin the room is a box, and another exit.";
+	//exit the hive 
+	// open the box and get the medkit
+	public static final String SCENARIO_25 = "The player exits the room to find himself back outside the spaceship where he began his searching."
+			+ "\n The player realizes the dangers of the planet and goes back to the entrance of the cave to get the other crew members. ";
+	//pic another entrance 
+	//or if it is the last cjoice go to finall.
+	
+	public static final String FINAL = "The player exits the room to find himself back outside the spaceship where he began his searching. "
+			+ "\nThe player realizes the dangers of the planet and goes back to the entrance of the cave to get the other crew members. ";
+	
+	
+	public static final String SCENARIO_27 = "";
+    public static final String SCENARIO_28 = "";
+	public static final String SCENARIO_29 = "";
+	public static final String SCENARIO_30 = "";
+	//last 10 rooms , with end game
 }
