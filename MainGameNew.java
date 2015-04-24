9163d6d84c50adf71e004b30973f5004e2b9258c
agguzman <a.guillermoguzman@gmail.com>
2015-04-24 14:05:14 -0400

    Made changes to the MainGame and Scenarios

--- MainGameNew.java
+++ MainGameNew.java
@@ -4,7 +4,7 @@ public class MainGameNew {
 
 	public static GameSaveAndLoad gs = new GameSaveAndLoad();
 	public static Scanner in = new Scanner(System.in);
-	public static String room = "1";
+	public static String room = "Room_1";
 	public static String userInput = "";
 
 	public static void main(String[] args) {
@@ -20,68 +20,199 @@ public class MainGameNew {
 			} else {
 				switch (room) {
 
-				case "1":
-					System.out.println(Senarios.SENARIO_1);
+				/**
+				 * 
+				 * 
+				 * ROOM 1
+				 * 
+				 * 
+				 */
+				case "Room_1":
+					System.out.println("\n" + Scenarios.SENARIO_1 + "\n");
 					room = "1A";
 					break;
 
 				case "1A":
 
-					switch (userInput = in.next()) {
+					// Only can go west[1]
+					switch (userInput = in.nextLine()) {
 
 					case "1":
-						room = "2";
+						room = "Room_2";
 						break;
 
 					default:
 						System.out
-								.println("\nI don't understand your request.");
+								.println("\nI don't understand your request.\n");
 						room = "1A";
 						break;
 					}
 
 					break;
 
-				case "2":
-					System.out.println(Senarios.SENARIO_2);
+				/**
+				 * 
+				 * 
+				 * ROOM 2
+				 * 
+				 * 
+				 */
+				case "Room_2":
+					System.out.println("\n" + Scenarios.SENARIO_2 + "\n");
 					room = "2A";
-					// gs.saveGame("1A");
+					// gs.saveGame("2A");
 					break;
 
 				case "2A":
 
-					switch (userInput = in.next()) {
+					switch (userInput = in.nextLine()) {
 
 					case "1":
-						System.out.println(Senarios.SENARIO_3);
+						room = "Room_3";
+
 						break;
 
 					case "2":
 						System.out
-								.println("\nThe right path is blocked right now, please pick another entrance.");
-						room = "2A";
+								.println("\nThe right path is blocked right now, please pick another entrance.\n");
+						room = "Room_2";
 						break;
 
 					case "3":
 
 						System.out
-								.println("\nThe straight path is blocked right now, please pick another entrance.");
-						room = "2A";
+								.println("\nThe straight path is blocked right now, please pick another entrance.\n");
+						room = "Room_2";
 						break;
 
 					default:
 						System.out
-								.println("\nI don't understand your request.");
-						room = "2";
+								.println("\nI don't understand your request.\n");
+						room = "Room_2";
 						break;
 
 					}
 
-				}
-			}
+					break;
+
+				/**
+				 * 
+				 * 
+				 * ROOM 3
+				 * 
+				 * 
+				 */
+				case ("Room_3"):
+					System.out.println("\n" + Scenarios.SENARIO_3 + "\n");
+					System.out.println("\n" + Puzzle.LIGHT_COMBINATION + "\n");
+
+					userInput = in.nextLine();
+					if (Puzzle.solvingLightCombination(userInput)) {
+						System.out.println("SOLVEDDD"); // for testing
+						room = "Room_4";
+					} else {
+						System.out.println("NNNOoo"); // testing
+						room = "2A";
+					}
+
+					break;
+
+				/**
+				 * 
+				 * 
+				 * ROOM 4
+				 * 
+				 */
+				case ("Room_4"):
+					System.out.println("\n" + Scenarios.SENARIO_4 + "\n");
+
+				case ("4A"):
+					switch (userInput = in.nextLine()) {
+
+					case ("1"):
+						room = "Room_5";
+						break;
+
+					default:
+						room = "4A";
+						break;
+					}
+
+					break;
+
+				/**
+				 * 
+				 * 
+				 * ROOM 5
+				 * 
+				 * 
+				 */
+				case ("Room_5"):
+					System.out.println(Scenarios.SENARIO_5);
+					break;
+				case ("5A"):
+					switch (userInput = in.nextLine()) {
+
+					case ("1"):
+						room = "Room_6";
+						break;
+
+					default:
+						room = "5A";
+						break;
+					}
+
+					break;
+
+				/**
+				 * 
+				 * 
+				 * ROOM 6
+				 * 
+				 * 
+				 */
+				case ("Room_6"):
+					System.out.println(Scenarios.SENARIO_6);
+					break;
+				case ("6A"):
+					switch (userInput = in.nextLine()) {
+
+					case ("1"):
+						room = "Room_7";
+						break;
+
+					default:
+						room = "6A";
+						break;
+					}
+
+					break;
+
+				/**
+				 * 
+				 * 
+				 * ROOM 7
+				 * 
+				 * 
+				 */
+				case ("Room_7"):
+					System.out.println("\n" + Scenarios.SENARIO_7 + "\n");
+					System.out.println("\n" + Puzzle.RIDDLE + "\n");
+
+					userInput = in.nextLine();
+					/*
+					 * if (Puzzle.solvingRiddle()) {
+					 * System.out.println("SOLVEDDD"); // for testing room =
+					 * "Room_4"; } else { System.out.println("NNNOoo"); //
+					 * testing room = "2A"; }
+					 */
+
+					break;
+
+				} // this brace is the room switch
+			} // else
 
 		}
 
 	}
-
 }
--- Scenarios.java
+++ Scenarios.java
@@ -1,24 +1,24 @@
-package thatNewWay;
+
 
 public class Scenarios {
 
-	public static final String INTRO = "A group of travelers make way to the newly found planet of unknown name. "
+	public static final String SENARIO_1 = "A group of travelers make way to the newly found planet of unknown name. "
 			+ "\nThe planet is covered with massive forest and grassy terrain. "
-			+ "\nA plethora of colors excite the traveler’s eyes as they fly over the plant to find a landing place.  "
+			+ "\nA plethora of colors excite the travelerï¿½s eyes as they fly over the plant to find a landing place.  "
 			+ "\nMixture of blues, purples, indigos, violets, and aqua cover the planet in its beauty. "
 			+ "\nAs the ship lands it is determined that a group of 4 will exit the space craft and search the planet. "
 			+ "\nThe plane lands and the group of 4 exit the craft, leaving the others on the craft. "
 			+ "\nAll the followers go into different directions. "
-			+ "\nYou are standing on the grassy plain and the only way for you to go is west.";
+			+ "\nYou are standing on the grassy plain and the only way for you to go is west. \n[1]GO WEST";
 
-	public static final String ROOM2 = "Walking west of the spacecraft, the YOU admire the beauty of the planet. "
+	public static final String SENARIO_2 = "Walking west of the spacecraft, the YOU admire the beauty of the planet. "
 			+ "\nAs you continue to walk your team runs into a mountain covered with vines of blue and aqua as well as blue trees with indigo leaves and violet trees. "
 			+ "\nLooking at the mountain straight-forward you see three entrances and all of them have different marking above the entrance. "
 			+ "\nThe left entrance is curved in shape and has an emblem right above it. The middle entrance is square in shape and has a group of bees above it. "
-			+ "\nThe right entrance is triangle in shape and has a stream of glowing plasma flowing above the “V” shaped entrance."
+			+ "\nThe right entrance is triangle in shape and has a stream of glowing plasma flowing above the ï¿½Vï¿½ shaped entrance."
 			+ "\n[1] GO LEFT" + "\n[2] GO RIGHT" + "\n[3] GO STRAIGHT";
 
-	public static final String ROOM3 = "Walking to the left, you walk through the curved shaped entrance into the pitch black cave. "
+	public static final String SENARIO_3 = "Walking to the left, you walk through the curved shaped entrance into the pitch black cave. "
 			+ "\nAs you pass the entrance a door made of rock slams shut behind you, trapping you in the cave.  "
 			+ "\nYou become agitated with the occurrence and continues to walk forward. "
 			+ "\nContinuing the walk down the dark cave, you see a thin white light and begin to run towards it. "
@@ -27,13 +27,13 @@ public class Scenarios {
 			+ "The balls flash in a certain order blue, blue, aqua, green, purple, green. The flashing starts again: aqua, purple... "
 			+ "\nThe flashing stops and then starts back again with two blue. ";
 
-	public static final String ROOM4 = "Another door opens up and reveals the other side of the mountain. "
+	public static final String SENARIO_4 = "Another door opens up and reveals the other side of the mountain. "
 			+ "\nYou move forward and the door slams shut behind. "
 			+ "\nyou, run in to the spaceship again and the road west back towards the entrance to the mountain. "
 			+ "\n(You made a huge circle and must continue around in another entrance. "
 			+ "\nGo west and Into another entrance ";
 
-	public static final String ROOM5 = "The player takes the right entrance with plasma. "
+	public static final String SENARIO_5 = "The player takes the right entrance with plasma. "
 			+ "\nOnce, the you walk past the entrance a field of plasma stretches over "
 			+ "\nthe entrance and the cave is now lit by plasma streams along the wall. "
 			+ "\nThe cave is decorated with mossy rocks and the floor is covered with purple grass. "
@@ -42,17 +42,17 @@ public class Scenarios {
 	// a.Continue on through the cave
 	// b.Go into the room
 
-	public static final String ROOM6 = "After entering the room, you decide, "
+	public static final String SENARIO_6 = "After entering the room, you decide, "
 			+ "nothing is in the room and get a weird feeling about leaving. ";
 
 	// a.Go further into the room
 	// b.Leave the room immediately
 
-	public static final String ROOM6_2 = "You listen to your gut and start back for the entrance. "
+	public static final String ROOM_6_2 = "You listen to your gut and start back for the entrance. "
 			+ "The entrance begins to get farther and farther away. "
 			+ "The room closes in on you and you sufocate. {Game over}  ";
 
-	public static final String ROOM7 = "The player decides to stay walking further into the room and comes face to face with the light. "
+	public static final String SENARIO_7 = "The player decides to stay walking further into the room and comes face to face with the light. "
 			+ "\nThe light shatters and the room begins to close in on the player."
 			+ "\nThe pieces of light surround the player and then move in front of the player. "
 			+ "\nA message of light begins to appear and the message turns out to be a riddle."
@@ -67,7 +67,7 @@ public class Scenarios {
 			+ "\nType 'add' to add the items to your inventory. ";
 
 	public static final String ROOM8 = "Continuing down the cave, the player is introduced to a plethora of plants all of which are omitting different smells. "
-			+ "\nOne particular plant catches the player’s eye. "
+			+ "\nOne particular plant catches the playerï¿½s eye. "
 			+ "\nThe plant is pinks in color with purple stripes, blue dots and resembles a dahlia flower. "
 			+ "\nThe player decides to ignore the smell and continues down the cave and runs into Jack Sheppard from the space ship."
 			+ "\nJack is freaking out for some reason and speaking in riddles. He begins to get mad at you for not understanding him and swings at the player. "
